881d702aeee07a1b02daa948bd9738df3959db24
Lipika <lipikabiswal.lb@gmail.com>
2021-08-01 19:19:02 +0530

    commiting first spring project

---
+++ GGIt/First_Spring_boot_proj/src/main/java/com/in28minutes/springboot/service/WelcomeService.java
@@ -0,0 +1,14 @@
+package com.in28minutes.springboot.service;
+
+
+import org.springframework.stereotype.Component;
+
+
+	@Component
+	public class WelcomeService {
+
+		public String retrieveWelcomeMessage() {
+			//Complex Method
+			return "Good Morning updated";
+		}
+}
